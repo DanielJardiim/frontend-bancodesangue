f32e03025c9816b0832f4ebff6ac293be0f34885
DanielJardiim <jn_dani@outlook.com>
2022-09-29 11:39:35 -0300

    deleted:    src/index.css         modified:   src/index.js         modified:   src/pages/CadastroPage/index.js         modified:   src/pages/CadastroPage/styles.css

--- src/pages/CadastroPage/index.js
+++ src/pages/CadastroPage/index.js
@@ -1,7 +1,72 @@
+import './styles.css';
+
 const CadastroPage = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
+    };
+
     return (
-        <div className="cadastroPage">
-            <h1>Cadastro</h1>
+        <div className="cadastro-page">
+            <div className="header">
+                <h1>CADASTRO</h1>
+            </div>
+            <div className="cadastro-container">
+                <div className="entrar-dados">
+                    <div className="nome">
+                        <label>Nome:</label>
+                        <input type="text" name="nome" id="nome" required />
+                    </div>
+                    <div className="endereco">
+                        <label>Endereço:</label>
+                        <input
+                            type="text"
+                            name="endereco"
+                            id="endereco"
+                            required
+                        />
+                    </div>
+                    <div className="telefone">
+                        <label>telefone:</label>
+                        <input
+                            type="text"
+                            name="telefone"
+                            id="telefone"
+                            required
+                        />
+                    </div>
+                    <div className="email">
+                        <label>Email:</label>
+                        <input type="email" name="email" id="email" required />
+                    </div>
+                    <div className="tipo-sanguineo">
+                        <label>Tipo Sanguineo:</label>
+                        <input
+                            type="text"
+                            name="tipo-sanguineo"
+                            id="tipo-sanguineo"
+                            required
+                        />
+                    </div>
+                    <div className="senha">
+                        <label>Senha:</label>
+                        <input
+                            type="password"
+                            name="senha"
+                            id="senha"
+                            required
+                        />
+                    </div>
+                </div>
+                <div className="cadastrar-btn">
+                    <button
+                        className="btn-cadastro"
+                        type="submit"
+                        onClick={handleSubmit}
+                    >
+                        CADASTRAR
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
