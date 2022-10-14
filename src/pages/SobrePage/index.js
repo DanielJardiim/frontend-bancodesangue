f9947dcd2ea45d1e84f4706fcbd67b8f917c966d
Vinicius <viniciusdeoliveira16@hotmail.com>
2022-10-14 16:42:06 -0300

    Ajustando o sobre

--- src/pages/SobrePage/index.js
+++ src/pages/SobrePage/index.js
@@ -16,6 +16,29 @@ const SobrePage = () => {
                 <div className="header">
                     <h1 className="h1-banco">SOBRE</h1>
                 </div>
+                <div className="Finalidade">
+                    <h1 className="h1">Finalidade do Projeto</h1>
+                    <h1>
+                            O projeto teve como objetivo, instruir acerca da doação de sangue no Brasil, a fim do facilitamento
+                            da doação de sangue pelos cidadões.
+                    </h1>
+                </div>
+                <div className="Integrantes">
+                    <h1 className="h1">Integrantes do Grupo</h1>
+                    <h1>
+                            Nome: Vinícius de Oliveira Azevedo - 1526 / Daniel Jardim Nunes - 1525<br></br>
+                            Curso: Engenharia de Computação
+                    </h1>
+                </div>
+                <div className="Doadores">
+                    <h1 className="h1">Quem doa para quem:</h1>
+                    <h1>
+                            A+ (doa para) -> A+ / AB+ <br></br> A- (doa para) -> A+ / AB+ / A- / AB- <br></br>
+                            B+ (doa para) -> B+ / AB+ <br></br> B- (doa para) -> B+ / AB+ / B- / AB- <br></br>
+                            AB+ (doa para) -> AB+ <br></br> AB- (doa para) -> AB+ / AB- <br></br>
+                            O+ (doa para) -> A+ / AB+ / B+ / O+ <br></br> O- (doa para) -> Todos <br></br>
+                    </h1>
+                </div>
                 <button
                     className="btn-sobre"
                     type="submit"
