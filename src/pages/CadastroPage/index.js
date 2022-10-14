0c77f2cd6d3ef837524dd4461e2ca821120b5b57
Vinicius <viniciusdeoliveira16@hotmail.com>
2022-10-14 09:50:53 -0300

    Subindo

--- src/pages/CadastroPage/index.js
+++ src/pages/CadastroPage/index.js
@@ -1,8 +1,16 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import './styles.css';
 
 const CadastroPage = () => {
     const navigate = useNavigate();
+
+    const [nome, SetNome] = useState("")
+    const [endereco, SetEndereco] = useState("")
+    const [telefone, SetTelefone] = useState("")
+    const [email, SetEmail] = useState("")
+    const [tipoSanguineo, SetTipoSanguineo] = useState("")
+    const [senha, SetSenha] = useState("")
 
     const handleSubmit = e => {
         e.preventDefault();
@@ -19,7 +27,13 @@ const CadastroPage = () => {
                 <div className="entrar-dados">
                     <div className="nome">
                         <label>Nome:</label>
-                        <input type="text" name="nome" id="nome" required />
+                        <input 
+                        type="text" 
+                        name="nome" 
+                        id="nome" 
+                        value={nome}
+                        onChange = { e => SetNome(e.target.value)}
+                        required />
                     </div>
                     <div className="endereco">
                         <label>Endereço:</label>
@@ -27,6 +41,8 @@ const CadastroPage = () => {
                             type="text"
                             name="endereco"
                             id="endereco"
+                            value={endereco}
+                            onChange = { e => SetEndereco(e.target.value)}
                             required
                         />
                     </div>
@@ -36,12 +52,20 @@ const CadastroPage = () => {
                             type="text"
                             name="telefone"
                             id="telefone"
+                            value={telefone}
+                            onChange = { e => SetTelefone(e.target.value)}
                             required
                         />
                     </div>
                     <div className="email">
                         <label>Email:</label>
-                        <input type="email" name="email" id="email" required />
+                        <input 
+                        type="email" 
+                        name="email" 
+                        value={email}
+                        onChange = { e => SetEmail(e.target.value)}
+                        id="email" 
+                        required />
                     </div>
                     <div className="tipo-sanguineo">
                         <label>Tipo Sanguineo:</label>
@@ -49,6 +73,8 @@ const CadastroPage = () => {
                             type="text"
                             name="tipo-sanguineo"
                             id="tipo-sanguineo"
+                            value={tipoSanguineo}
+                            onChange = { e => SetTipoSanguineo(e.target.value)}
                             required
                         />
                     </div>
@@ -57,6 +83,8 @@ const CadastroPage = () => {
                         <input
                             type="password"
                             name="senha"
+                            value={senha}
+                            onChange = { e => SetSenha(e.target.value)}
                             id="senha"
                             required
                         />
