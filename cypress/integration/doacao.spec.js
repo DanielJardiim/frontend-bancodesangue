c473b58ddb2335e4aa5fce03cf69415ad8915e7a
guilherme <guilhermedmlopes@hotmail.com>
2023-06-12 20:06:05 -0300

    Adicionando teste de login. Para rodar os teste npm run test

--- cypress/integration/doacao.spec.js
+++ cypress/integration/doacao.spec.js
@@ -2,12 +2,20 @@
 
 describe('Doacao', () => {
     it('devem poder realizar um cadastro', () => {
-    cy.visit('http://localhost:3000/cadastro');
-    cy.get('[id=nome]').type('Vinicius');
+    cy.visit('http://localhost:3001/cadastro');
+    cy.get('[id=nome]').type('Joao');
     cy.get('[id=endereco]').type('Av. João de Camargo, 510 - Centro');
     cy.get('[id=telefone]').type('35 99999-1111');
-    cy.get('[id=email]').type('vinicius@inatel.br');
+    cy.get('[id=email]').type('joao@inatel.br');
     cy.get('[id=tipo-sanguineo]').type('A+');
     cy.get('[id=senha]').type('123456');
+    });
+});
+
+describe('Login', () => {
+    it('devem poder realizar um login', () => {
+    cy.visit('http://localhost:3001/');
+    cy.get('[id=email]').type('testuser@hotmail.com');
+    cy.get('[id=password]').type('testuser123');   
     });
 });
